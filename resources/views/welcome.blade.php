4000ca812d53a0d559b4afc559af9658821ecc4f
yashvantha <yashvanth.hj@gmail.com>
2017-01-17 19:27:57 +0530

    welcomepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel its 5.3
                 </div>
 
                 <div class="links">
